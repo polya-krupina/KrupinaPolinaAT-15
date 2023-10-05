d95ca6f927612449f4be737361c428b5ee1d0c77
polya <polyakrupina@gmail.com>
2023-10-05 18:04:58 +0500

    -

--- buns/mod2/task11.py
+++ buns/mod2/task11.py
@@ -1,7 +1,15 @@
-s = input()
+'''s = input()
 result = False
 for i in range(10):
     if s.count(str(i)) > 1:
         result = True
         break
+print(result)'''
+s = input()
+result = False
+for i in range(len(s)):
+    for j in range(i+1, len(s)):
+        if s[i] == s[j] and s[i] != ' ':
+            result = True
+            break
 print(result)
