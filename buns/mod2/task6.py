9a63b94bf0ac5a3294c3589f594b074b59989e55
polya <polyakrupina@gmail.com>
2023-10-09 14:46:25 +0500

    -

--- buns/mod2/task6.py
+++ buns/mod2/task6.py
@@ -1,9 +1,9 @@
-s = input()
+s = input()[::-1]
 current_domain = ''
 for i in range(len(s)):
     if s[i] == '.':
-        print(current_domain)
+        print(current_domain[::-1])
         current_domain = ''
     else:
         current_domain += s[i]
-print(current_domain)
+print(current_domain[::-1])
