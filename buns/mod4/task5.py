f794e70f3d97bb551bb1ba060368e61a4d76d61a
polya <polyakrupina@gmail.com>
2023-11-08 21:33:14 +0500

    -

--- buns/mod4/task5.py
+++ buns/mod4/task5.py
@@ -1,4 +1,6 @@
-text = open('input.txt').readline()
+input_file = open('input.txt')
+text = input_file.readline()
+input_file.close()
 letters = list(filter(lambda symbol: symbol.isalpha(), set(text)))
 statistic = {}
 for letter in letters:
@@ -7,3 +9,4 @@ statistic = sorted(statistic.items(), key=lambda x: x[1])
 output_file = open('output.txt', 'w')
 for i in range(len(statistic)):
     output_file.write(statistic[i][0] + ': ' + str(statistic[i][1]) + '\n')
+output_file.close()
